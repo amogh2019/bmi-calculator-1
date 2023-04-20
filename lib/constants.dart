38348e0efa8279d652f412eb0602b78f67b01599
amogh2019 <56151951+amogh2019@users.noreply.github.com>
2023-04-20 05:03:46 +0000

    update constatnts

---
+++ lib/constants.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const kPaddingAll10 = EdgeInsets.all(10);
+const kAppNameText = Text('BMI CALCULATOR');
+const kIconButtonCardSize = 32.0;
+const kMaxHumanHeight = 240.0;
+const kMinHumanHeight = 80.0;
